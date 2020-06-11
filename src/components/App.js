5dbe458d1690daff5a879bf159b14023d443731c
Racksoup <39200478+Racksoup@users.noreply.github.com>
2020-06-11 13:31:37 -0400

    React_Webpack_Starter

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  state = {
+    score: 0
+  }
+
+  addOne = () => {
+    console.log('clicked');
+    this.setState({
+      score: this.state.score + 1
+    })
+  }
+  
+  render() {
+    return (
+      <div>
+        <h1>My React App</h1>
+        <button onClick={this.addOne}>Add One</button>
+        <p> {this.state.score} </p>
+      </div>
+    )
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  devServer: {
+    contentBase: path.resolve(__dirname, "./src"),
+    historyApiFallback: true
+  },
+  entry: path.resolve(__dirname, "./src/index.js"),
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: "babel-loader"
+      }
+    ]
+  },
+  output: {
+    filename: "bundle.js"
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
+}
